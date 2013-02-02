e2ae598725289cade7955b16ca20f43e7848d5ff
Nicholas Burlett <nickburlett@mac.com>
2013-02-02 11:57:55 -0800

    fix some test errors caused by configuration

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -40,4 +40,15 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.request', # request context processor is needed
+    'django.contrib.messages.context_processors.messages',
 )
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+)
+
